bfff8d6a24b9b347194eee910c5f79bba7c97ab1
Anwar Zaid <mnanwarmz@gmail.com>
2023-01-13 21:58:13 +0800

    facilities get works with test

--- routes/api.php
+++ routes/api.php
@@ -16,10 +16,10 @@ use Illuminate\Support\Facades\Route;
 // Authentication
 Route::post('login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
 Route::post('logout', [\App\Http\Controllers\Api\AuthController::class, 'logout']);
-
+Route::get('login', [\App\Http\Controllers\Api\AuthController::class, 'login_page'])->name('login');
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('facilities', \App\Http\Controllers\Api\FacilitiesController::class);
+    Route::get('facilities', [\App\Http\Controllers\Api\FacilitiesController::class, 'index']);
 });
--- tests/Feature/Api/FacilitiesTest.php
+++ tests/Feature/Api/FacilitiesTest.php
@@ -16,16 +16,27 @@ class FacilitiesTest extends TestCase
      */
     public function test_api_get_facilities()
     {
+        $this->withoutExceptionHandling();
+        // Login
+        $user = \App\Models\User::factory()->create([
+            'password' => bcrypt('password')
+        ]);
+        // Login user
+        $response = $this->post('/api/login', [
+            'email' => $user->email,
+            'password' => 'password'
+        ]);
+        $this->assertAuthenticated();
         // Create 10 facilities
         $facilities = \App\Models\Facility::factory()->count(10)->create();
         // Get facilities
         $response = $this->get('/api/facilities');
+        // Assert status code
+        $response->assertStatus(200);
+
         // Assert return json data
-        dd($response);
         $response->assertJson([
             'data' => $facilities->toArray()
         ]);
-        // Assert return status code
-        $response->assertStatus(200);
     }
 }
