015b11c70591ba10ee6667b8772fc058d19564f6
Anwar Zaid <mnanwarmz@gmail.com>
2023-01-16 12:55:08 +0800

    created reservation model, controller, migrations

---
+++ database/migrations/2023_01_16_045131_create_reservations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('facility_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->string('purpose');
+            $table->string('status')->default('pending');
+            $table->string('remarks')->nullable();
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
